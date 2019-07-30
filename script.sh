a789c64b720f1645539609f5987bae30e35a295b
joedicandia <dicandiajoseph@gmail.com>
2019-07-30 01:04:13 +0000

    completed script

--- script.sh
+++ script.sh
@@ -3,8 +3,6 @@
 while read motif
 do
 echo $motif
-trimmedmotif=$motif | tr -d [:space:]
-grep -o $trimmedmotif "$2" | wc -l
+trimmedmotif="$(echo $motif | tr -d [:space:])"
+grep -o $trimmedmotif $2 | wc -l
 done < "$1"
-
-
