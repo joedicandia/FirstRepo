98e89b696e065205fba536d1022a2108c9e80fb7
joedicandia <dicandiajoseph@gmail.com>
2019-07-31 01:15:13 +0000

    finished maybe

--- script.sh
+++ script.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+# Goal: Generate new file that prints the number of occurences of a motif in a fasta file. Create a new fasta file for each motif, listing all of teh genes and sequences containing that motif; name each new file after the motif and output to the directory "motifs"
+# Author: Joe DiCandia
+
+# Search fasta file for motifs:
+
 while read motif
 do
 echo $motif
 trimmedmotif="$(echo $motif | tr -d [:space:])"
 grep -o $trimmedmotif $2 | wc -l
 done < "$1" > motif_count.txt
+
+# Create motifs directory:
+
+mkdir motifs
+
+# Add new files to motif directory containing genes witht that motif:
+
+
+while read motif
+do
+trimmedmotif2="$(echo $motif | tr -d [:space:])"
+grep -i $trimmedmotif2 $2 >> "motifs/$trimmedmotif2.txt"
+done < "$1"
